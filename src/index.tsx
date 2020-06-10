73b53065c6d8a17abd15f9f20ea20e6e64c4793d
Jason <690163223@qq.com>
2020-06-10 16:00:06 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// let hello: string = "hello Typescript"
+// document.querySelectorAll(".app")[0].innerHTML = hello
+
+import React from "react"
+import ReactDOM from "react-dom"
+import Hello from "./components/demo/Hello"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Hello name="typescript!"></Hello>,
+  document.querySelectorAll(".app")[0]
+)
